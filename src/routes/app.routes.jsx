29a5de7116a33239fb76eac760b5a5f225df87c8
Felipe Borba <felipe.borba0109@gmail.com>
2023-11-20 19:31:45 -0300

    add page navigation

---
+++ src/routes/app.routes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "../pages/Home";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* <Route path="/dish/create" element={<DishCreate />} />
+      <Route path="/dish/view/:id" element={<DishView />} />
+      <Route path="/dish/update/:id" element={<DishUpdate />} /> */}
+    </Routes>
+  );
+};
