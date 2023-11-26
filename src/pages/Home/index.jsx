c2671e2cd7adc8da48a48ab3839272aca9cdad12
Felipe Borba <felipe.borba0109@gmail.com>
2023-11-26 00:06:53 -0300

    home bg

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,13 @@
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import Main from "../../components/Main";
+import "./style.css";
 
 export const Home = () => {
   return (
     <>
+      <div className="homeBgGirl" />
+      <div className="dna" />
       <Header />
       <Main>
         <h1>teste</h1>
