a666ca30d93b9ab4fa96b4c95dfbaff3af7114eb
Felipe Borba <felipe.borba0109@gmail.com>
2023-11-25 21:21:40 -0300

    style header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,3 +1,31 @@
-export const Header = () => {
-  return <header>header</header>;
-};
+import "./style.css";
+import flowLogo from "../../assets/flow-logo-small.svg";
+import ButtonLink from "../Button/ButtonLink";
+import ButtonPrimary from "../Button/ButtonPrimary";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={flowLogo} alt="flow logo" />
+
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/sobre">Sobre nós</a>
+          </li>
+          <li>
+            <a href="/">Download App</a>
+          </li>
+        </ul>
+      </nav>
+
+      <div>
+        <ButtonLink>Fazer cadastro</ButtonLink>
+        <ButtonPrimary>Entrar</ButtonPrimary>
+      </div>
+    </header>
+  );
+}
