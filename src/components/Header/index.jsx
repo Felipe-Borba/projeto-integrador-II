e15eeedfc5e62bc90f25bcc21e32e2e718483203
André Luís <71388515+DreLuis@users.noreply.github.com>
2023-11-28 19:33:09 -0300

    Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,12 +2,15 @@ import "./style.css";
 import flowLogo from "../../assets/flow-logo-small.svg";
 import ButtonLink from "../Button/ButtonLink";
 import ButtonPrimary from "../Button/ButtonPrimary";
-import { userNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
   return (
     <header>
-      <img src={flowLogo} alt="flow logo" />
+      <a href="/">
+        <img src={flowLogo} alt="flow logo" />
+      </a>
 
       <nav>
         <ul>
@@ -24,7 +27,7 @@ export default function Header() {
       </nav>
 
       <div>
-        <ButtonLink onClick={() => Navigate("/cadastro")}>Fazer cadastro</ButtonLink>
+        <ButtonLink onClick={() => navigate("/cadastro")}>Fazer cadastro</ButtonLink>
         <ButtonPrimary>Entrar</ButtonPrimary>
       </div>
     </header>
