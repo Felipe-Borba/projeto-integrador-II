77c428d73a4950ab28f0780999541e11f24ba71e
BRUNO HENRIQUE SOUZA <bruno_henrique_souza@estudante.sc.senai.br>
2023-11-28 20:14:50 -0300

    about

---
+++ src/pages/Sobre/index.jsx
@@ -0,0 +1,26 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import Main from "../../components/Main";
+import flowLogo from "../../assets/flow-logo-big.png";
+import "./style.css";
+
+export const Sobre = () => {
+
+    return (
+    <>
+        <div className="homeBgGirlAbout" />
+        <Header/>
+        <Main>
+            <div className="flowImgLogoStyle"><img className="flowImgLogo" src={flowLogo} alt="Entre no flow" /></div>
+            
+            <div className="aboutUs">
+                <p><h1>Sobre nós</h1>
+
+
+
+                </p>
+            </div>
+        </Main>
+        <Footer/>
+    </>);
+};
--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Cadastro } from "../pages/Cadastro";
+import { Sobre } from "../pages/Sobre";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/cadastro" element={<Cadastro />}/>
+      <Route path="/cadastro" element={<Cadastro />} />
+      <Route path="/sobre" element={<Sobre />} />
       {/* <Route path="/dish/create" element={<DishCreate />} />
       <Route path="/dish/view/:id" element={<DishView />} />
       <Route path="/dish/update/:id" element={<DishUpdate />} /> */}
