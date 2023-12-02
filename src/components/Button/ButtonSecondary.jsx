14dcd30cd900645ec5238146b026c5941601ef50
Felipe Borba <felipe.borba0109@gmail.com>
2023-12-02 17:14:23 -0300

    fix button style

--- src/components/Button/ButtonSecondary.jsx
+++ src/components/Button/ButtonSecondary.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./style.css";
 
-export default function ButtonSecondary({ onClick, children }) {
+export default function ButtonSecondary({ children, ...rest }) {
   return (
-    <button className="buttonSecondary" onClick={onClick}>
+    <button className="buttonSecondary" {...rest}>
       {children}
     </button>
   );
