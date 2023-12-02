1ee2fdf3eec32d0dee4b773a82f5cef83ac683ca
BRUNO HENRIQUE SOUZA <bruno_henrique_souza@estudante.sc.senai.br>
2023-12-01 21:20:17 -0300

    about page done v1

--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -1,8 +1,13 @@
+import React from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import Main from "../../components/Main";
 import flowLogo from "../../assets/flow-logo-big.png";
 import "./style.css";
+import homeCardReview1 from "../../assets/about/about-card-review-1.png";
+import homeCardReview2 from "../../assets/about/about-card-review-2.png";
+import homeCardReview3 from "../../assets/about/about-card-review-3.png";
+import homeCardReview4 from "../../assets/about/about-card-review-4.png";
 
 export const Sobre = () => {
 
@@ -13,22 +18,45 @@ export const Sobre = () => {
         <Main>
             <div className="flowImgLogoStyle"><img className="flowImgLogo" src={flowLogo} alt="Entre no flow" /></div>
             
-            <div className="aboutUs">
+            <div className="aboutAboutUs">
                 <p><h1>SOBRE NÓS</h1></p>
 
-                <div className="infoBox">
-                    <p><h3>Texto na caixa preta</h3>
-                    <br />
+                <div className="aboutInfoBox">
+                    <p>
                     Lorem ipsum dolor sit amet. Ut fugiat aliquid nam consectetur perferendis et facilis voluptas vel deserunt minima est dolore recusandae et corporis
                     earum hic atque recusandae! Ea consequuntur voluptas non fugiat consectetur a impedit illo!
                     Et sint necessitatibus ex dolorem dolorem sed quis enim id enim quibusdam! Est quia praesentium ad velit quia ut accusantium 
                     quos qui vero dolores sit atque quia. Et fugit esse ad maxime corrupti non nihil veritatis non debitis amet non minus quibusdam.
                     Ut ullam omnis et tempore quibusdam rem molestiae numquam. Aut ullam ducimus ad voluptatem optio est delectus galisum ab inventore illum. 
                     Ut vero possimus est odit repellat eum distinctio nemo et dolor enim ut labore libero. Id perspiciatis cupiditate a voluptatibus sunt eum dicta reiciendis qui
-                     sunt esse qui doloribus enim aut ullam omnis sed incidunt asperiores.
+                    sunt esse qui doloribus enim aut ullam omnis sed incidunt asperiores. Lorem ipsum dolor sit amet. Ut fugiat aliquid nam consectetur perferendis et facilis voluptas vel deserunt minima est dolore recusandae et corporis
+                    earum hic atque recusandae! Ea consequuntur voluptas non fugiat consectetur a impedit illo!
                     </p>
+
+                    <div className="aboutIconContainer">
+                        <div class="aboutIcon">
+                            <img src={homeCardReview1} alt="icon1" />
+                        </div>
+                        <div class="aboutIcon">
+                            <img src={homeCardReview2} alt="icon2" />
+                        </div>
+                        <div class="aboutIcon">
+                            <img src={homeCardReview3} alt="icon3" />
+                        </div>
+                        <div class="aboutIcon">
+                            <img src={homeCardReview4} alt="icon4" />
+                        </div>
+
+                    </div>
+
+                    <div className="aboutIconContainer">
+                        <div class="aboutIconText">Bruno H</div>
+                        <div class="aboutIconText">Felipe B</div>
+                        <div class="aboutIconText">André L</div>
+                        <div class="aboutIconText">Leonardo M</div>
+
+                    </div>
                 </div>
-                
             </div>
         </Main>
         <Footer/>
