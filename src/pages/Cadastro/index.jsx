66bb117cfdaefe378c2df8e29d3759a533f6510a
Felipe Borba <felipe.borba0109@gmail.com>
2023-12-02 11:55:29 -0300

    remove duplicated css and fix minor things

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -1,22 +1,21 @@
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import Main from "../../components/Main";
-import React, { useState } from 'react';
-import "./style.css"
+import React, { useState } from "react";
+import "./style.css";
 import Input from "../../components/Input";
 import ButtonPrimary from "../../components/Button/ButtonPrimary";
 import ButtonSecondary from "../../components/Button/ButtonSecondary";
 
-
 export const Cadastro = () => {
   const [formData, setFormData] = useState({
-    nome: '',
-    email: '',
-    senha: '',
-    confirmarSenha: '',
+    nome: "",
+    email: "",
+    senha: "",
+    confirmarSenha: "",
   });
 
-  const [erroSenha, setErroSenha] = useState('');
+  const [erroSenha, setErroSenha] = useState("");
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -25,45 +24,70 @@ export const Cadastro = () => {
       [name]: value,
     }));
 
-    if (name === 'senha' || name === 'confirmarSenha') {
+    if (name === "senha" || name === "confirmarSenha") {
       setErroSenha(
         formData.senha !== formData.confirmarSenha
-          ? 'As senhas não coincidem'
-          : ''
+          ? "As senhas não coincidem"
+          : ""
       );
     }
-  }  
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Dados do formulário enviados:', formData);
+    console.log("Dados do formulário enviados:", formData);
   };
 
   return (
-  <>
-    <div className="homeBgGirl" />
-    <div className="dna" />
-    <Header/>
-    <Main>
-      <div className="bloco"> 
-        <form onSubmit={handleSubmit}>
-          <h2>Cadastro</h2>
-          <Input label={"Usuario"} type={"name"}value={formData.nome} onChange={handleChange}/>
-          <Input label={"Email"} type={"email"} value={FormData.email} onChange={handleChange}/>
-          <Input label={"Senha"} type={"password"} value={FormData.senha} onChange={handleChange}>
-            <p>Senha <span>(mínimo 6 caracteres)</span></p>
-          </Input>
-          <Input label={"Confirmar Senha"} type={"password"} value={FormData.confirmarSenha} onChange={handleChange}>
-            <p>Senha <span>(mínimo 6 caracteres)</span></p>
-          </Input>
-          
-          <p style={{ color: 'red' }}>{erroSenha}</p>
-          <ButtonSecondary>Salvar</ButtonSecondary>
-        </form>
-      </div>
-    </Main>
-    <Footer/>
-  </>);
+    <>
+      <div className="homeBgGirl" />
+      <div className="homeBgDna" />
+      <Header />
+      <Main>
+        <div className="signIn">
+          <form onSubmit={handleSubmit}>
+            <h2>Cadastro</h2>
+            <Input
+              label={"Usuario"}
+              type={"name"}
+              value={formData.nome}
+              onChange={handleChange}
+            />
+            <Input
+              label={"Email"}
+              type={"email"}
+              value={FormData.email}
+              onChange={handleChange}
+            />
+            <Input
+              label={"Senha"}
+              type={"password"}
+              value={FormData.senha}
+              onChange={handleChange}
+            >
+              <p>
+                Senha <span>(mínimo 6 caracteres)</span>
+              </p>
+            </Input>
+            <Input
+              label={"Confirmar Senha"}
+              type={"password"}
+              value={FormData.confirmarSenha}
+              onChange={handleChange}
+            >
+              <p>
+                Senha <span>(mínimo 6 caracteres)</span>
+              </p>
+            </Input>
+
+            <p style={{ color: "red" }}>{erroSenha}</p>
+            <ButtonSecondary>Salvar</ButtonSecondary>
+          </form>
+        </div>
+      </Main>
+      <Footer />
+    </>
+  );
 };
 
-export default Cadastro;  
+export default Cadastro;
--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -10,55 +10,67 @@ import homeCardReview3 from "../../assets/about/about-card-review-3.png";
 import homeCardReview4 from "../../assets/about/about-card-review-4.png";
 
 export const Sobre = () => {
-
-    return (
+  return (
     <>
-        <div className="homeBgGirlAbout" />
-        <Header/>
-        <Main>
-            <div className="flowImgLogoStyle"><img className="flowImgLogo" src={flowLogo} alt="Entre no flow" /></div>
-            
-            <div className="aboutAboutUs">
-                <p><h1>SOBRE NÓS</h1></p>
+      <div className="homeBgGirl" />
+      <Header />
+      <Main>
+        <div className="flowImgLogoStyle">
+          <img className="flowImgLogo" src={flowLogo} alt="Entre no flow" />
+        </div>
 
-                <div className="aboutInfoBox">
-                    <p>
-                    Lorem ipsum dolor sit amet. Ut fugiat aliquid nam consectetur perferendis et facilis voluptas vel deserunt minima est dolore recusandae et corporis
-                    earum hic atque recusandae! Ea consequuntur voluptas non fugiat consectetur a impedit illo!
-                    Et sint necessitatibus ex dolorem dolorem sed quis enim id enim quibusdam! Est quia praesentium ad velit quia ut accusantium 
-                    quos qui vero dolores sit atque quia. Et fugit esse ad maxime corrupti non nihil veritatis non debitis amet non minus quibusdam.
-                    Ut ullam omnis et tempore quibusdam rem molestiae numquam. Aut ullam ducimus ad voluptatem optio est delectus galisum ab inventore illum. 
-                    Ut vero possimus est odit repellat eum distinctio nemo et dolor enim ut labore libero. Id perspiciatis cupiditate a voluptatibus sunt eum dicta reiciendis qui
-                    sunt esse qui doloribus enim aut ullam omnis sed incidunt asperiores. Lorem ipsum dolor sit amet. Ut fugiat aliquid nam consectetur perferendis et facilis voluptas vel deserunt minima est dolore recusandae et corporis
-                    earum hic atque recusandae! Ea consequuntur voluptas non fugiat consectetur a impedit illo!
-                    </p>
+        <div className="aboutAboutUs">
+          <p>
+            <h1>SOBRE NÓS</h1>
+          </p>
 
-                    <div className="aboutIconContainer">
-                        <div class="aboutIcon">
-                            <img src={homeCardReview1} alt="icon1" />
-                        </div>
-                        <div class="aboutIcon">
-                            <img src={homeCardReview2} alt="icon2" />
-                        </div>
-                        <div class="aboutIcon">
-                            <img src={homeCardReview3} alt="icon3" />
-                        </div>
-                        <div class="aboutIcon">
-                            <img src={homeCardReview4} alt="icon4" />
-                        </div>
+          <div className="aboutInfoBox">
+            <p>
+              Lorem ipsum dolor sit amet. Ut fugiat aliquid nam consectetur
+              perferendis et facilis voluptas vel deserunt minima est dolore
+              recusandae et corporis earum hic atque recusandae! Ea consequuntur
+              voluptas non fugiat consectetur a impedit illo! Et sint
+              necessitatibus ex dolorem dolorem sed quis enim id enim quibusdam!
+              Est quia praesentium ad velit quia ut accusantium quos qui vero
+              dolores sit atque quia. Et fugit esse ad maxime corrupti non nihil
+              veritatis non debitis amet non minus quibusdam. Ut ullam omnis et
+              tempore quibusdam rem molestiae numquam. Aut ullam ducimus ad
+              voluptatem optio est delectus galisum ab inventore illum. Ut vero
+              possimus est odit repellat eum distinctio nemo et dolor enim ut
+              labore libero. Id perspiciatis cupiditate a voluptatibus sunt eum
+              dicta reiciendis qui sunt esse qui doloribus enim aut ullam omnis
+              sed incidunt asperiores. Lorem ipsum dolor sit amet. Ut fugiat
+              aliquid nam consectetur perferendis et facilis voluptas vel
+              deserunt minima est dolore recusandae et corporis earum hic atque
+              recusandae! Ea consequuntur voluptas non fugiat consectetur a
+              impedit illo!
+            </p>
 
-                    </div>
-
-                    <div className="aboutIconContainer">
-                        <div class="aboutIconText">Bruno H</div>
-                        <div class="aboutIconText">Felipe B</div>
-                        <div class="aboutIconText">André L</div>
-                        <div class="aboutIconText">Leonardo M</div>
-
-                    </div>
-                </div>
+            <div className="aboutIconContainer">
+              <div class="aboutIcon">
+                <img src={homeCardReview1} alt="icon1" />
+              </div>
+              <div class="aboutIcon">
+                <img src={homeCardReview2} alt="icon2" />
+              </div>
+              <div class="aboutIcon">
+                <img src={homeCardReview3} alt="icon3" />
+              </div>
+              <div class="aboutIcon">
+                <img src={homeCardReview4} alt="icon4" />
+              </div>
             </div>
-        </Main>
-        <Footer/>
-    </>);
+
+            <div className="aboutIconContainer">
+              <div class="aboutIconText">Bruno H</div>
+              <div class="aboutIconText">Felipe B</div>
+              <div class="aboutIconText">André L</div>
+              <div class="aboutIconText">Leonardo M</div>
+            </div>
+          </div>
+        </div>
+      </Main>
+      <Footer />
+    </>
+  );
 };
