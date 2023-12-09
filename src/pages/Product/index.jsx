b642f4fa7f17355260152385df0b373bb2610cce
Bruno Henrique Souza <xbrunoohs@gmail.com>
2023-12-08 21:36:38 -0300

    component product page and fix phone image

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import homeCardReview1 from "../../assets/about/about-card-review-1.png";
-import homeCardReview2 from "../../assets/about/about-card-review-2.png";
-import homeCardReview3 from "../../assets/about/about-card-review-3.png";
-import homeCardReview4 from "../../assets/about/about-card-review-4.png";
 import flowLogo from "../../assets/flow-logo-big.svg";
 import homeBg from "../../assets/home/background.png";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import Main from "../../components/Main";
 import "./style.css";
+import ProductText from "./ProductText";
+import flowPhone from "../../assets/product/iphonescreen.png";
 
 export const Product = () => {
   return (
@@ -28,32 +26,16 @@ export const Product = () => {
           </p>
 
           <div className="productInfoBox">
-            <img className="productPhoneImage" src="\src\assets\product\iphonescreen.png" alt="failed" />
-            
-            <div className="productText">
-              <h1>PROBLEMA</h1>
-              <p>
-              Lorem ipsum dolor sit amet. Ut fugiat aliquid nam consectetur
-              perferendis et facilis voluptas vel deserunt minima est dolore
-              recusandae et corporis earum hic atque recusandae! Ea consequuntur
-              voluptas non fugiat consectetur a impedit illo! Et sint
-              necessitatibus ex dolorem dolorem sed quis enim id enim quibusdam!
-              Est quia praesentium ad velit quia ut accusantium quos qui vero
-              dolores sit atque quia. Et fugit esse ad maxime corrupti non nihil
-              <h1>PROPOSTA</h1>
-              Lorem ipsum dolor sit amet. Ut fugiat aliquid nam consectetur
-              perferendis et facilis voluptas vel deserunt minima est dolore
-              recusandae et corporis earum hic atque recusandae! Ea consequuntur
-              voluptas non fugiat consectetur a impedit illo! Et sint
-              necessitatibus ex dolorem dolorem sed quis enim id enim quibusdam!
-              <h1>OBJETIVO</h1>
-              Lorem ipsum dolor sit amet. Ut fugiat aliquid nam consectetur
-              perferendis et facilis voluptas vel deserunt minima est dolore
-              recusandae et corporis earum hic atque recusandae! Ea consequuntur
-              voluptas non fugiat consectetur a impedit illo! Et sint
-              necessitatibus ex dolorem dolorem sed quis enim id enim quib
+            <img
+              className="productPhoneImage"
+              src={flowPhone}
+              alt="failed"
+            />
 
-              </p>
+            <div className="productText">
+              <ProductText title={"PROBLEMA"} content={"A falta de acesso a informações consolidadas e centralizadas dificulta a busca por opções de lazer. Isso afeta a capacidadede tomar decisões informadas e aproveitar ao máximo as atividades e oportunidades disponíveis na cidade. Isso não apenas limita as experiências de lazer, mas também contribuipara a sensação de desconexão e frustração, destacando a necessidade de soluções que simplifiquem e centralizem o acesso a informações sobre eventos na comunidade."}/>
+              <ProductText title={"PROPOSTA"} content={"Desenvolver um website que centraliza informações sobre shows, eventos e atrações, incorporando avaliações e média de preços, ofereceria uma solução eficaz para a dificuldade de encontraropções de lazer. Essa plataforma abrangente simplificaria a busca por eventos, permitindo aos usuários filtrar por preferências específicas. Além disso, funcionalidades de avaliações e média de preços promoveriam a transparência e o engajamento com a comunidade, proporcionando uma experiência mais rica e satisfatória para os usuários, ao mesmo tempo em que impulsiona o apoio a eventos locais e estimula a economia regional."}/>
+              <ProductText title={"OBJETIVO"} content={"Buscamos garantir uma experiência facilitada para os usuários acessarem informações sobre eventos, com o objetivo de tornar a busca por diversão mais prática e eficiente. Além de proporcionar praticidade imediata, nossa plataforma visa contribuir para a formação de uma comunidade engajada, conectada e enriquecida culturalmente. Queremos oferecer um meio intuitivo para os usuários explorarem diversas opções de lazer, fortalecendo assim os laços sociais e a vitalidade da comunidade local."}/>
             </div>
           </div>
         </div>
